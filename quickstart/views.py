c672abdb913a8dd05e12857490f7a5f65e909553
harut <harutyun.ghubasaryan@betconstruct.com>
2018-01-15 20:37:55 +0400

    promoted filter added

--- quickstart/views.py
+++ quickstart/views.py
@@ -12,11 +12,16 @@ class Fruits(generics.ListCreateAPIView):
     serializer_class = FruitSerializer
 
     def get_queryset(self):
+        queryset = Fruit.objects.language(self.request.query_params.get('lang', None))
         fruit_id = self.kwargs.get('pk', None)
+        promoted = self.request.query_params.get('promoted', None)
         if fruit_id is not None:
-            queryset = Fruit.objects.language(self.request.query_params.get('lang', None)).filter(id=fruit_id)
+            queryset = queryset.filter(id=fruit_id)
+        elif promoted is not None:
+            queryset = queryset.filter(promoted=promoted)
         else:
-            queryset = Fruit.objects.language(self.request.query_params.get('lang', None)).all()
+            queryset.language(self.request.query_params.get('lang', None)).all()
+
         return queryset
 
 
